a1132a41cc14cd28fd74abf962155333ebf98bfe
Kibolino <112396097+Kibolino@users.noreply.github.com>
2022-12-19 16:27:14 -0300

    Add files via upload

--- src/Sobre.js
+++ src/Sobre.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import imgmockup from "./img/mockup.png";
+import "./SobreStyle.css";
 
 export default function Sobre() {
   return (
-    <div>Sobre</div>
+    <div className='conteiner'>
+      <img class="img" src={imgmockup} alt="Mockup do projeto do site" />
+        <article class="artigo">
+          <h2>Mockup do projeto</h2>
+          <br />
+          <p>
+            O projeto de site Farejador foi criado com objetivo de 
+            obter pontos na matéria de Desenvolvimento de Sistema Web,
+            ministrada por Professor Doutor Eduardo Ribeiro Felipe.
+          </p>
+          <br />
+          <p>
+            
+          </p>
+        </article>
+    </div>
   )
 }
