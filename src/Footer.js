4c5114bd25868dbe8eb8416cfa8848ce7efe8226
Kibolino <112396097+Kibolino@users.noreply.github.com>
2022-12-19 13:20:34 -0300

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './FooterStyle.css';
+
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <div class="footer">
+          &copy;<span> 2022 </span>
+          <span> Farejador. Todos os direitos reservados.</span>
+        </div>
+      </footer>
+    </div>
+  );
+}
